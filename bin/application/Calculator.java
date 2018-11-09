725da505d469a0d7d3cab8592d71e352c69d80f7
dorian.ljuta <dori.ljuta@hotmail.com>
2018-11-09 08:02:59 +0100

    Your Message

---
+++ bin/application/Calculator.java
@@ -0,0 +1,64 @@
+package application;
+
+/**
+ * Berechnet das Formelrad
+ * @author Peter Rutschmann
+ * @version 13.09.2018
+ */
+public class Calculator {
+	private double leistung;
+	private double spannung;
+	private double strom;
+	private double widerstand;
+	
+	public Calculator(double leistung, double spannung, double strom, double widerstand) {
+		super();
+		this.leistung = leistung;
+		this.spannung = spannung;
+		this.strom = strom;
+		this.widerstand = widerstand;
+	}
+	
+	public double getLeistung() {
+		return leistung;
+	}
+	
+	public double getSpannung() {
+		return spannung;
+	}
+
+	public double getStrom() {
+		return strom;
+	}
+
+	public double getWiderstand() {
+		return widerstand;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculator [leistung=" + leistung + 
+				", spannung=" + spannung + 
+				", strom=" + strom + 
+				", widerstand="	+ widerstand + "]";
+	}
+
+	public void calculate() {
+		this.leistung = pAusUundR(this.spannung, this.widerstand);
+		this.strom = iAusUundR(this.spannung, this.widerstand);
+	}
+	
+	public double pAusUundR (double u, double r) {
+		
+		return (u*u)/r;
+	}
+	
+	public double iAusUundR (double u, double r) {
+		
+		return u/r;
+	
+	}
+	/* Hier die Methoden mit den Formlen hinzufügen
+	 */
+	
+}
