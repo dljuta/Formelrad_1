5ab938be1262b81ea3f2b12b080ef53cff15a9cb
CORPROOT\TAAELMI2 <Michel.Elias@Swisscom.com>
2018-11-21 19:30:11 +0100

    clean

--- bin/application/Calculator.java
+++ bin/application/Calculator.java
@@ -2,6 +2,7 @@ package application;
 
 /**
  * Berechnet das Formelrad
+ * 
  * @author Peter Rutschmann
  * @version 13.09.2018
  */
@@ -10,7 +11,7 @@ public class Calculator {
 	private double spannung;
 	private double strom;
 	private double widerstand;
-	
+
 	public Calculator(double leistung, double spannung, double strom, double widerstand) {
 		super();
 		this.leistung = leistung;
@@ -18,11 +19,11 @@ public class Calculator {
 		this.strom = strom;
 		this.widerstand = widerstand;
 	}
-	
+
 	public double getLeistung() {
 		return leistung;
 	}
-	
+
 	public double getSpannung() {
 		return spannung;
 	}
@@ -37,68 +38,64 @@ public class Calculator {
 
 	@Override
 	public String toString() {
-		return "Calculator [leistung=" + leistung + 
-				", spannung=" + spannung + 
-				", strom=" + strom + 
-				", widerstand="	+ widerstand + "]";
+		return "Calculator [leistung=" + leistung + ", spannung=" + spannung + ", strom=" + strom + ", widerstand="
+				+ widerstand + "]";
 	}
 
 	public void calculate() {
 		this.leistung = pAusUundR(this.spannung, this.widerstand);
 		this.strom = iAusUundR(this.spannung, this.widerstand);
 	}
-	
-	public double pAusUundR (double u, double r) {
+
+	public double pAusUundR(double u, double r) {
 		System.out.println("(u*u)/r");
-		return (u*u)/r;
+		return (u * u) / r;
 	}
-	
-	public double iAusUundR (double u, double r) {
+
+	public double iAusUundR(double u, double r) {
 		System.out.println("u/r");
-		return u/r;
-	
+		return u / r;
+
 	}
-	
-	public double pAusUundI (double u, double i) {
+
+	public double pAusUundI(double u, double i) {
 		System.out.println("u*i");
-		return u*i;
+		return u * i;
 	}
 
-	public double rAusUundI (double u, double i) {
+	public double rAusUundI(double u, double i) {
 		System.out.println("u/i");
-		return u/i;
+		return u / i;
 	}
-	
-	public double pAusRundI (double i, double r) {
+
+	public double pAusRundI(double i, double r) {
 		System.out.println("(i*i)*r");
-		return (i*i)*r;
+		return (i * i) * r;
 	}
-	
-	public double UAusRundI (double r, double i) {
+
+	public double UAusRundI(double r, double i) {
 		System.out.println("r*i");
-		return r*i;
-	}
-	
-
-	public double iAusPundR (double p, double r) {
-		System.out.println("Math.sqrt(p/r)");
-		return Math.sqrt(p/r);
-	}
-	
-	public double UAusRundP (double r, double p) {
-		System.out.println("Math.sqrt(p/r)");
-		return Math.sqrt(p/r);
+		return r * i;
 	}
 
-	public double iAusAundB (double p, double u) {
+	public double iAusPundR(double p, double r) {
+		System.out.println("Math.sqrt(p/r)");
+		return Math.sqrt(p / r);
+	}
+
+	public double UAusRundP(double r, double p) {
+		System.out.println("Math.sqrt(p/r)");
+		return Math.sqrt(p / r);
+	}
+
+	public double iAusAundB(double p, double u) {
 		System.out.println("p/u");
-		return p/u;
-	}
-	
-	public double rAusAundB (double p, double u) {
-		System.out.println("(u*u)/p");
-		return (u*u)/p;
+		return p / u;
 	}
 
-	
+	public double rAusAundB(double p, double u) {
+		System.out.println("(u*u)/p");
+		return (u * u) / p;
+	}
+
 }
